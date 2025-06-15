e6986269d66b077f7c25c2faf83b0f283da091ec
pasi5838 <rikitidore39@gmail.com>
2025-06-15 16:05:34 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('tebak-hewan-cache').then(function(cache) {
+      return cache.addAll([
+        './',
+        './index.html',
+        './style.css',
+        './script.js',
+        './questions.js',
+        './icon.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
